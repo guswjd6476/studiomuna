19c4f9e10af069e1a656e56b92b79e73646b6a29
hyunjung <sspp4555@naver.com>
2024-02-04 22:47:26 +0900

    sitemap update

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    images: {
+        loader: 'imgix',
+        path: ['https://www.studiomoona.co.kr/'],
+    },
 };
+
 module.exports = nextConfig;
