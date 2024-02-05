cdc648186f68157c1c5f397ede4b4b64afe1ace3
hyunjung <sspp4555@naver.com>
2024-02-06 01:16:16 +0900

    debug

--- src/pages/Program.tsx
+++ src/pages/Program.tsx
@@ -13,6 +13,12 @@ const Program = () => {
         { date: '2023.08', title: '무나 여름나기(계곡)' },
     ];
 
+    const array = [
+        { title: '프로그램 연혁', path: '/1' },
+        { title: '프로그램 소개', path: '/2' },
+        { title: '저시기머시기', path: '/3' },
+    ];
+
     const programIntroductions = [
         {
             title: '오마(음)카세',
@@ -70,8 +76,29 @@ const Program = () => {
                     content="스튜디오무나, 스튜디오, 무나,  studiomoona, STUDIOMOONA, 가치사전, 가치오락,"
                 />
             </Head>
-            <InnerHead title={'MOONA PROGRAM'} />
+            <InnerHead
+                title={'MOONA PROGRAM'}
+                array={array}
+            />
             <div className="max-w-4xl mx-auto">
+                <div>
+                    <h2 className="text-4xl font-bold mb-6">프로그램 연혁</h2>
+                    <div className="relative">
+                        {programs.map((program, index) => (
+                            <div
+                                key={index}
+                                className="mb-10"
+                            >
+                                <div className="flex items-center mb-4">
+                                    <div className="bg-indigo-600 text-white rounded-full p-2 mr-4">{program.date}</div>
+                                    <div>
+                                        <h3 className="text-xl font-bold">{program.title}</h3>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
                 {/* 프로그램 소개 섹션 */}
                 <h2 className="text-4xl font-bold mb-6">프로그램 소개</h2>
                 <div className="relative">
